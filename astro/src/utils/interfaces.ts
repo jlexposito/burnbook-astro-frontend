c82443d30fd72e0812416cc552447316e7eef639
Jose Exposito <jlexposito7@gmail.com>
2023-03-21 01:19:45 +0100

    Remove unused code and add interfaces to improve typing. Add missing components into recipe view

---
+++ astro/src/utils/interfaces.ts
@@ -0,0 +1,17 @@
+export interface Ingredient {
+    name: string;
+    quantity: number;
+    unit: string;
+    prefix: string;
+}
+
+export interface Recipe {
+    title: string;
+    ingredients: Array<Ingredient>;
+    servings: number;
+    image: string;
+    cooking_time: number;
+    instructions: string;
+    references: Array<string>;
+    tags: Array<string>;
+}
