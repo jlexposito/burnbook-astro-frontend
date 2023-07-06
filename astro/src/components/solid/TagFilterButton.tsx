a4dcf888e6f370f44ce4b9e4ffb95ee4d9b08b7b
Jose Exposito <jlexposito7@gmail.com>
2023-07-07 00:13:03 +0200

    Fix outline issue in buttons and bump dependencies

--- astro/src/components/solid/TagFilterButton.tsx
+++ astro/src/components/solid/TagFilterButton.tsx
@@ -15,7 +15,7 @@ export default function Tag(props: {active: boolean, tagname: string, clearButto
     }
 
     function activeClassName() {
-        return props.active ?  'active flex flex-row' : '';
+        return props.active ?  'active ' : '';
     }
 
     return (
@@ -30,7 +30,7 @@ export default function Tag(props: {active: boolean, tagname: string, clearButto
             </button> : 
             <button
                 onClick={ ev => setFilter(props.tagname, ev)}
-                class={`${activeClassName()} tag bk-accent bk-accent-hover`}
+                class={`${activeClassName()} flex flex-row tag bk-accent bk-accent-hover`}
             >
                 <Show when={props.active}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M9 16.17L4.83 12l-1.42 1.41L9 19L21 7l-1.41-1.41z"/></svg>
