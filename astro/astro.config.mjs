9b5c693aa124c76b70a7b93a373124d6707c6dbc
José Luis Expósito Robles <jlexposito7@gmail.com>
2026-02-27 08:29:50 +0100

    Add sitemap integration and site URL
    Added sitemap integration and updated site URL.

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,19 +1,21 @@
-import { defineConfig } from 'astro/config'
 import Compress from 'astro-compress'
-import icon from 'astro-icon'
-import solidJs from '@astrojs/solid-js'
+import { defineConfig } from 'astro/config'import icon from 'astro-icon'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
+import sitemap from '@astrojs/sitemap';
+import solidJs from '@astrojs/solid-js'
 
 export default defineConfig({
+  site: 'https://burnbook.zh0nb.com',
   devToolbar: {
     enabled: false,
   },
-
+  
   integrations: [
     icon(),
     solidJs(),
     Compress(),
+    sitemap()
   ],
 
   vite: {
