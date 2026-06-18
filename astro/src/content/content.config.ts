3055c9e984271de8a00bf1c089e6e120a58b1e1f
Jose Exposito <jlexposito7@gmail.com>
2026-06-18 12:55:07 +0200

    Add missing import

--- astro/src/content/content.config.ts
+++ astro/src/content/content.config.ts
@@ -1,5 +1,6 @@
 // Import utilities from `astro:content`
 import { defineCollection } from "astro:content";
+import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
 const Ingredient = z.object({
